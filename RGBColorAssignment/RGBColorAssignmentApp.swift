b29359d5bff4ffbc3b2e8577eabeec9c4d6fc147
AmalHassanAlmaabady <amalalmaabady@gmail.com>
2022-12-20 14:37:25 +0300

    Initial Commit

---
+++ RGBColorAssignment/RGBColorAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBColorAssignmentApp.swift
+//  RGBColorAssignment
+//
+//  Created by Eman on 26/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct RGBColorAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
